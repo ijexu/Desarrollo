3d0a76d9716919d8b10bdc96b49d21cedbbebf65
ijexu <jesus.labrana@mail.udp.cl>
2026-09-25 19:23:03 -0300

    Se crea y se estructura main.html, este siendo la pagina principal del casino, se crea un javascript para crear un menu desplegable

---
+++ Frontend/main.js
@@ -0,0 +1,6 @@
+const caja_desplegable = document.getElementById('caja_desplagable');
+const boton_despliegue = document.getElementById('boton_despliegue');
+
+boton_despliegue.addEventListener('click', () => {
+	boton.despliegue.classList.toggle('collapsed');
+});
